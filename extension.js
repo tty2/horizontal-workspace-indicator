1ac803ebc87b02a9efe199f9370475c77a8b9699
Roman Suvorov <suvorov83@gmail.com>
2020-12-21 19:42:36 +0300

    construct widget text dynamically

--- extension.js
+++ extension.js
@@ -1,8 +1,11 @@
-const St = imports.gi.St;
-const Clutter = imports.gi.Clutter;
+const { Clutter, St } = imports.gi;
 const Main = imports.ui.main;
 
+const bullet = "●";
+const circle = "○";
+
 let panelButton, panelButtonText;
+
 
 function init() {
     panelButton = new St.Bin({
@@ -10,11 +13,25 @@ function init() {
     });
     panelButtonText = new St.Label({
         style_class: "panel-button-text",
-        text: "●○○",
+        text: getWidgetText(),
         x_expand: true,
         y_align: Clutter.ActorAlign.CENTER,
     });
     panelButton.set_child(panelButtonText);
+}
+
+function getWidgetText() {
+    let txt = "";
+    let numberWorkspaces = global.workspace_manager.get_n_workspaces();
+    let currentWorkspaceIndex = global.workspace_manager.get_active_workspace_index();
+    for (let i = 0; i < numberWorkspaces; i++) {
+        if (i == currentWorkspaceIndex) {
+            txt += bullet;
+        } else {
+            txt += circle;
+        }
+    }
+    return txt
 }
 
 function enable() {
