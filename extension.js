a655c8bea2f87111c9009ef0f7dacf391df0f562
Roman Suvorov <suvorov83@gmail.com>
2020-12-22 21:53:54 +0300

    make indicator as a class

--- extension.js
+++ extension.js
@@ -1,24 +1,56 @@
-const { Clutter, St } = imports.gi;
+const { Clutter, St, GObject } = imports.gi;
 const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
 
 const bullet = "●";
 const circle = "○";
 
-let panelButton, panelButtonText;
+let _indicator;
 
+let WorkspaceIndicator = GObject.registerClass(
+    class WorkspaceIndicator extends PanelMenu.Button {
+        _init() {
+            super._init(0.0, _('Workspace Indicator'));
+            
+            let container = new St.Widget({
+                layout_manager: new Clutter.BinLayout(),
+                x_expand: true,
+                y_expand: true,
+            });
+            this.add_actor(container);
+            
+            this._statusLabel = new St.Label({
+                style_class: "panel-button-text",
+                text: getWidgetText(),
+                x_expand: true,
+                y_align: Clutter.ActorAlign.CENTER,
+            });
+            container.add_actor(this._statusLabel);
 
-function init() {
-    panelButton = new St.Bin({
-        style_class: "panel-button"
-    });
-    panelButtonText = new St.Label({
-        style_class: "panel-button-text",
-        text: getWidgetText(),
-        x_expand: true,
-        y_align: Clutter.ActorAlign.CENTER,
-    });
-    panelButton.set_child(panelButtonText);
-}
+            let workspaceManager = global.workspace_manager;
+            this._workspaceManagerSignals = [
+                workspaceManager.connect_after('notify::n-workspaces',
+                    this._nWorkspacesChanged.bind(this)),
+                workspaceManager.connect_after('workspace-switched',
+                    this._onWorkspaceSwitched.bind(this)),
+            ];
+
+        }
+
+        _onDestroy() {
+            super._onDestroy();
+        }
+
+        _onWorkspaceSwitched() {
+            this._statusLabel.text = getWidgetText();
+        }
+    
+        _nWorkspacesChanged() {
+            this._statusLabel.text = getWidgetText();
+        }
+    }
+);
 
 function getWidgetText() {
     let txt = "";
@@ -34,10 +66,14 @@ function getWidgetText() {
     return txt
 }
 
+function init() {
+}
+
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(panelButton, 1);
+    _indicator = new WorkspaceIndicator();
+    Main.panel.addToStatusArea('workspace-indicator', _indicator);
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(panelButton);
+    _indicator.destroy();
 }
